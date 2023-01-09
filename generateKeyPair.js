8617fe9089b2f85dbc8f6b510d014d21df1cf1fc
Luca Donazzon <ldonazzon@griffeshield.com>
2023-01-09 23:08:34 +0100

    restore

--- generateKeyPair.js
+++ generateKeyPair.js
@@ -3,7 +3,7 @@ nacl.util = require('tweetnacl-util');
 
 const generateKeyPair = () => {
     const mykey = nacl.sign.keyPair()
-    return { publicKey: nacl.util.encodeBase64(mykey.publicKey), secretKey: nacl.util.encodeBase64(mykey.secretKey) }
+    return { publicKey: Buffer.from(mykey.publicKey).toString('hex'), secretKey: Buffer.from(mykey.secretKey).toString('hex') }
 }
 
 console.log("keyPair:", generateKeyPair())
