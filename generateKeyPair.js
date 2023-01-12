deef77ded1190065889e0cb650587f237d4d72a1
Luca Donazzon <ldonazzon@griffeshield.com>
2023-01-12 11:05:00 +0100

    yml

--- generateKeyPair.js
+++
@@ -1,9 +0,0 @@
-import nacl from 'tweetnacl';
-nacl.util = require('tweetnacl-util');
-
-const generateKeyPair = () => {
-    const mykey = nacl.sign.keyPair()
-    return { publicKey: Buffer.from(mykey.publicKey).toString('hex'), secretKey: Buffer.from(mykey.secretKey).toString('hex') }
-}
-
-console.log("keyPair:", generateKeyPair())
